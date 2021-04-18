c052fe144536817b259458b44cc38c1dc4066bac
maryainduri <induri@trinnylondon.com>
2021-04-18 08:47:11 +0100

    first commit with simple login feature

---
+++ README.md
@@ -0,0 +1,8 @@
+# cypress-orangelive-example
+
+This repository contains Cypress OrangeLiveDemo app e2e tests
+
+# contacts
+**name:** Marya Induri
+
+[marya.induri@gmail.com]()
---
+++ cypress/integration/orangelive/hRManagement.spec.js
@@ -0,0 +1,23 @@
+describe('hr should be able to add person to system', function () {
+
+    beforeEach(() => {
+        cy.visit("/");
+        cy.get("#txtUsername").should("be.visible")
+            .log("OrangeLive App login page has been loaded");
+    });
+
+    it('should be able to add employee details', function () {
+
+        cy.get("#txtUsername").type("Admin")
+            .log("Username entered");
+
+        cy.get("#txtPassword").type("admin123")
+            .log("Password has been entered");
+
+        cy.get("#btnLogin").click()
+            .log("Login button clicked");
+
+        cy.url().should("include","/dashboard")
+            .log("Dashboard has been loaded, login OK");
+    });
+});
